e6a5b8e1d04a53e29f5fc96290f797c9af74e32e
Md-razvi <mraza1748@gmail.com>
2025-09-25 02:09:54 +0530

    Task completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
-
 public class Main {
+    public static void main(String [] args){
+        Product p=new Product();
+        int a=p.product(12,12);
+        int b=p.product(5,6,7);
+        double c=p.product(12.5,13.0);
+        System.out.println(a+" "+b+" "+c);
+
+
+
+    }
+}
+class Product {
+    public int product(int x,int y){
+        return  x+y;
+    }
+    public int product(int x,int y,int z){
+        return x+y+z;
+    }
+    public double product(double x, double y) {
+        return x+y;
+    }
 
 }
